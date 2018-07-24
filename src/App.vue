0c7b72f37e92cb4abfbe90a5e2debe2d95e40ce8
bloody <kirill310896@gmail.com>
2018-07-25 04:46:53 +0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="uk-background-fixed">
+  <div class="uk">
+      <div class="uk-card-header">
+          <div class="menu">
+            <ul class="uk-table-hover uk-animation-slide-top" data-uk-tab>
+            <li class="uk-active"><a href="">Главная</a></li>
+            <li><a href="">Сережа</a></li>
+            <li><a href="">Один из этих</a></li>
+            </ul>
+          </div>
+      </div>
   </div>
+    </div>
 </template>
 
 <script>
@@ -15,7 +24,19 @@ export default {
 </script>
 
 <style lang="less">
-
   @import "~uikit/src/less/uikit.theme.less";
+
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+    body {
+      background-color: darkgray;
+    }
+  ul, uk-tab {
+    background-color: black;
+  }
 
 </style>
