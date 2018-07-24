a2f3aea5b081819f0f7717bb971031061ff5ac3f
bloody <kirill310896@gmail.com>
2018-07-25 00:39:45 +0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/><
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -15,6 +15,7 @@ export default {
 </script>
 
 <style lang="less">
-  @import "node_modules/uikit/src/less/uikit.theme.less";
+
+  @import "~uikit/src/less/uikit.theme.less";
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
 
 Vue.config.productionTip = false
 
