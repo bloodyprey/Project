f5b8a4caad41212cac08a63ea1528bc8ee9bb31e
bloody <kirill310896@gmail.com>
2018-07-25 00:23:59 +0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/><
   </div>
 </template>
 
@@ -14,13 +14,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  @import "node_modules/uikit/src/less/uikit.theme.less";
+
 </style>
