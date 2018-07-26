e29d4f7d11226de5409929a676b0a4eb08073461
bloody <kirill310896@gmail.com>
2018-07-27 01:54:48 +0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="uk-background-fixed">
-  <div class="uk">
-      <div class="uk-card-header">
-          <div class="menu">
-            <ul class="uk-table-hover uk-animation-slide-top" data-uk-tab>
-            <li class="uk-active"><a href="">Главная</a></li>
-            <li><a href="">Сережа</a></li>
-            <li><a href="">Один из этих</a></li>
-            </ul>
-          </div>
-      </div>
-  </div>
+    <div class="uk uk-navbar">
+        <ul>Главная</ul>
+        <ul>Главная</ul>
+        <ul>Главная</ul>
+        <ul>Главная</ul>
+        <ul>Главная</ul>
     </div>
 </template>
 
@@ -25,18 +19,21 @@ export default {
 
 <style lang="less">
   @import "~uikit/src/less/uikit.theme.less";
-
-  html, body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
+  div, uk-navbar {
+      background-color: black;
+      position:relative;
+      float:left;
+      margin:0 10px;
+      height:40px;
   }
-    body {
-      background-color: darkgray;
-    }
-  ul, uk-tab {
-    background-color: black;
+  body {
+      overflow: auto;
+      height: 100vh;
+      background-color: black;
+      background-image: url("assets/fon-warcraft1.jpg");
+      background-position: center;
+      background-size: cover;
   }
 
-</style>
+
+    </style>
